5853c54e43749c35e484e3d1188ca9844c59c538
Abhishek kumar yadav <144767744+abhishek91174@users.noreply.github.com>
2024-07-23 13:57:48 +0530

    msgg

--- counter01/src/App.jsx
+++ counter01/src/App.jsx
@@ -2,11 +2,29 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+
+  const numbers = [1, 2, 3, 4, 5];
+
+// Define a callback function
+function square(number) {
+    return number * number;
+}
+
+// Use the map method with the callback function
+const squaredNumbers = numbers.map(square);
+
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
   const [counter, setCounter] = useState(15);
 
   const addValue = () => {
     if (counter < 20) {
-      setCounter(counter + 1);
+      setCounter(prevCounter=>prevCounter+1);
+      setCounter(prevCounter=>prevCounter+1);
+      setCounter(prevCounter=>prevCounter+1);
+      setCounter(prevCounter=>prevCounter+1);
+      setCounter(prevCounter=>prevCounter+1);
     }
   }
 
@@ -20,9 +38,9 @@ function App() {
     <>
       <h1>Hello Coders</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Increase Value</button>
+      <button onClick={addValue}>Increase Value:{counter}</button>
       <br />
-      <button onClick={subtractValue}>Decrease Value</button>
+      <button onClick={subtractValue}>Decrease Value:{counter}</button>
     </>
   )
 }
